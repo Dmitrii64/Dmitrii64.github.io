67d0d6135b70a6666e2c802987d662b764862ccc
Dmitrii64 <60173944+Dmitrii64@users.noreply.github.com>
2020-06-07 17:34:57 +0400

    Проект

---
+++ pages/contact us/index.js
@@ -0,0 +1,1 @@
+"use strict";var e=document.querySelector(".mobile-header_js"),t=document.querySelector(".header__menu_js"),o=document.querySelector(".mobile-header__button_js");t.addEventListener("click",function(){e.classList.add("mobile-header_open")}),o.addEventListener("click",function(){e.classList.remove("mobile-header_open")});
